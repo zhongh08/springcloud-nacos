3b1f139a854e9fb4c3ea113ea065d8fc8e0a7470
zhongh08 <zhongh08@163.com>
2020-08-24 23:20:35 +0800

    springcloud nacos sentinel code commit

--- nacos-provider/src/main/java/com/syoki/nacosprovider/controller/ProviderController.java
+++ nacos-provider/src/main/java/com/syoki/nacosprovider/controller/ProviderController.java
@@ -1,5 +1,6 @@
 package com.syoki.nacosprovider.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ public class ProviderController {
     Logger logger= LoggerFactory.getLogger(ProviderController.class);
 
     @GetMapping("/hi")
+    @SentinelResource(value="hi")
     public String hi(@RequestParam(value = "name",defaultValue = "forezp",required = false)String name){
 
         return "hi "+name;
